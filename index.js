ed01b927f73777071458abbb278d728f661b5e03
Eberechi <eberewill@gmail.com>
2021-06-12 00:47:29 +0100

    app server setup with request objects

---
+++ index.js
@@ -0,0 +1,50 @@
+/**
+ * API Primary file
+ */
+//Dependencies
+var http = require("http");
+var url = require("url");
+var stringDecoder = require("string_decoder").StringDecoder;
+// server should respond to all request with a string
+var server = http.createServer((req, res) => {
+  //get url and parse it
+  // var parsedUrl = new URL(req.url);
+  var parsedUrl = url.parse(req.url, true);
+
+  //get the path
+  var path = parsedUrl.pathname;
+  var trimmedPath = path.replace(/^\/+|\/+$/g, "");
+
+  //request method
+  var reqMethod = req.method.toLocaleLowerCase();
+
+  //request header
+  var reqHeader = req.headers;
+
+  //Get Request Querry
+  var querryStringObj = parsedUrl.query;
+
+  //handling request payload
+
+  var decoder = new stringDecoder("utf-8");
+
+  var buffer = "";
+  req.on("data", (data) => {
+    buffer += decoder.write(data);
+  });
+
+  req.on("end", () => {
+    buffer += decoder.end();
+
+    //send response
+    res.end("payload was recieved:  " + buffer);
+    //log it on console
+    console.log(buffer);
+  });
+});
+
+//start the server and have it listen to port 3000
+
+server.listen(3000, () => {
+  console.log("server listening to port 3000 now");
+});
